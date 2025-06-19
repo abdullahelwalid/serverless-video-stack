f5f78d0876ed8c3cd78e71535a6eef268643ca0a
Abdullah <abdallah.elmansoor@hotmail.com>
2025-06-20 03:06:28 +0400

    Updated variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,9 @@
 variable "s3-bucket-name" {
   type     = string
   nullable = false
-  default  = "videos-actioncamera-random"
 }
 
 variable "route53_domain" {
   type     = string
   nullable = false
-  default  = "abdullahelwalid.com"
 }
